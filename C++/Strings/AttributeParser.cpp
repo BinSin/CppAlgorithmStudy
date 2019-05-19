4c201d1be63924619f581e575629a21f486d5b7a
BinSin <binyong318@gmail.com>
2019-05-19 15:03:00 +0900

    String

---
+++ C++/Strings/AttributeParser.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N, Q;
+	cin >> N >> Q;
+
+	stringstream ss;
+	//for (int i = 0; i < N; i++) {
+		string query;
+		getline(cin, query);
+		//query.substr(1, query.length() - 2);
+		cout << query << endl;
+
+	//}
+
+	//for (int i = 0; i < Q; i++) {
+
+	//}
+
+	return 0;
+}
